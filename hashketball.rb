ede990e19bf4ccd76b4a7fff5339fba94cabd30f
JT Skidmore <johntoddskidmore@gmail.com>
2020-05-11 23:39:20 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -227,7 +227,7 @@ def player_numbers (team)
     if value[:team_name] == "Charlotte Hornets"
       
       counter = 0 
-      while counter < value[:players].length 
+      while counter < value[:players].length do
       
         numbers.push(value[:players][counter][:number])
         
